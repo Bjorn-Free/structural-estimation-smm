b3d729bd2342d396b1e86e27b0f610c42ecf6d25
Bjorn Jivung <b.jivung@gmail.com>
2026-03-04 14:17:39 +0100

    Initial project setup

---
+++ runSMM.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import scipy
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+def main():
+    print("✅ Environment OK")
+    print("numpy", np.__version__)
+    print("pandas", pd.__version__)
+    print("scipy", scipy.__version__)
+    print("statsmodels", sm.__version__)
+
+if __name__ == "__main__":
+    main()
